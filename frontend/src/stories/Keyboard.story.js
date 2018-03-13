7118830f2ed55c67d3013903bedce43a13333af4
Sebastian Springer <info@sebastian-springer.com>
2018-03-13 21:56:35 +0100

    fixed keyboard story

--- frontend/src/stories/Keyboard.story.js
+++ frontend/src/stories/Keyboard.story.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { decorateAction } from '@storybook/addon-actions';
 
 import { Keyboard } from '../Keyboard';
 
-storiesOf('Keyboard', module).add('display', () => <Keyboard />);
+storiesOf('Keyboard', module).add('display', () => (
+  <Keyboard handleClick={value => action('button clicked')} />
+));
