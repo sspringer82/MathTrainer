e68f411dddc296495397e9ee38735dc11a18cfd0
Sebastian Springer <info@sebastian-springer.com>
2018-04-09 22:06:34 +0200

    add configure store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Trainer } from './Trainer';
 import registerServiceWorker from './registerServiceWorker';
+import { configureStore } from './configureStore';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<Trainer />, document.getElementById('root'));
+const store = configureStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Trainer />
+  </Provider>,
+  document.getElementById('root'),
+);
 registerServiceWorker();
