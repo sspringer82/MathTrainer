66d8931c4fb9aca30d1f2d60c076bf5c9ea40a16
Sebastian Springer <info@sebastian-springer.com>
2018-04-09 22:05:49 +0200

    List of operations

---
+++ frontend/src/operations.reducer.js
@@ -0,0 +1,8 @@
+const operations = ['Addition', 'Subtraktion', 'Division', 'Multiplikation'];
+
+export const operationsReducer = (state = { operations }, action) => {
+  switch (action) {
+    default:
+      return state;
+  }
+};
