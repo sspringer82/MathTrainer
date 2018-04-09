76c18fc7e0ac3934f92da398f35861410bc3af4d
Sebastian Springer <info@sebastian-springer.com>
2018-04-09 22:06:25 +0200

    add operations reducer

--- frontend/src/configureStore.js
+++ frontend/src/configureStore.js
@@ -2,8 +2,10 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { combineEpics, createEpicMiddleware } from 'redux-observable';
 import createHistory from 'history/createBrowserHistory';
 import { routerReducer, routerMiddleware } from 'react-router-redux';
+import { operationsReducer } from './operations.reducer';
 
 const rootReducer = combineReducers({
+  operations: operationsReducer,
   router: routerReducer,
 });
 
