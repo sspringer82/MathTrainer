a28840b68a958ef26e9fb68366be2734dc4d1ce2
Sebastian Springer <info@sebastian-springer.com>
2020-03-04 22:09:10 +0100

    trainer

---
+++ src/Trainer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Keyboard from './Keyboard';
+
+interface Props {}
+
+const Trainer: React.FC<Props> = () => {
+  return (
+    <>
+      <Keyboard />
+    </>
+  );
+};
+
+export default Trainer;
