52398d25d1671d6c0683f8b26efe0f86f26b5862
Sebastian Springer <info@sebastian-springer.com>
2020-03-06 22:54:20 +0100

    addition

--- src/Task.ts
+++ src/Task.ts
@@ -1,11 +1,27 @@
-export Operator {
+export enum Operator {
   ADD,
   SUB,
   DIV,
-  MUL
-};
+  MUL,
+}
 
 export interface Task {
-  operands: number[],
-  operator: Operator,
+  operands: number[];
+  operator: Operator;
+  solution: number;
+}
+
+export function createAddition(): Task {
+  const op1 = createOperand(1);
+  const op2 = createOperand(1);
+
+  return {
+    operands: [op1, op2],
+    operator: Operator.ADD,
+    solution: op1 + op2,
+  };
+}
+
+function createOperand(digits: number) {
+  return Math.floor(Math.random() * 10 ** digits);
 }
