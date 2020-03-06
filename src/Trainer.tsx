0c6156edd4df8930fd8137fb9453a12514cd70ca
Sebastian Springer <info@sebastian-springer.com>
2020-03-06 23:00:21 +0100

    trainer

--- src/Trainer.tsx
+++ src/Trainer.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Keyboard from './Keyboard';
+import { Task, createAddition } from './Task';
 
 interface Props {}
 
 const Trainer: React.FC<Props> = () => {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  useEffect(() => {
+    setTasks(
+      Array(10)
+        .fill({})
+        .map(() => createAddition()),
+    );
+  }, []);
+
   return (
     <>
       <Keyboard />
